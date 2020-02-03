c0ff5a158fa5bb6b3cd1f2d251902c97de16e149
EmptyJackson <matthew@hmjackson.com>
2020-02-03 11:44:13 +0000

    Add dataset creation methods

---
+++ datasets.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def get_sequence_dataset(data):
+    patient_seqs = []
+    outcome_seqs = []
+    for patient_num in data['patient_nbr'].unique():
+        patient_data = data[data['patient_nbr']==patient_num]
+        patient_data = patient_data.sort_values(by=['encounter_id'])
+        patient_outcome = patient_data['time_in_hospital'].to_numpy()
+        patient_data = patient_data.drop(['patient_nbr', 'encounter_id'], axis=1)
+        patient_data = patient_data.to_numpy()
+        outcome_seqs.append(patient_outcome[1:].tolist())
+        patient_seqs.append(patient_data[:-1].tolist())
+    return patient_seqs, outcome_seqs
+
+def get_oneshot_dataset(data):
+    x, y = get_sequence_dataset(data)
+    x = [pat for seq in x for pat in seq]
+    y = [outcome for seq in y for outcome in seq]
+    return x, y
+
+from transformers import *
+if __name__ == '__main__':
+    x = main()
+    print(get_sequence_dataset(x))
+    print(get_oneshot_dataset(x))
